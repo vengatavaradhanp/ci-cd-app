2f25622a20199b34fd8795c8c4db6baec2a77e11
vengatavaradhanp <77961420+vengatavaradhanp@users.noreply.github.com>
2023-03-24 12:43:44 +0530

    Revert "feat: Added dev text"

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Dev
+          Learn React
         </a>
       </header>
     </div>
